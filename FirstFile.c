c4670fc7bc7cd344b9847f8c97784536fc195580
Alan Kraut <Alan.Kraut@gmail.com>
2018-09-09 22:01:03 -0400

    added first code

--- FirstFile.c
+++ FirstFile.c
@@ -1,6 +1,17 @@
 
 task main()
 {
+	motor[left] = 100;
+	motor[right] = 100;
+	wait1Msec(1000);
+
+	motor[left] = -10;
+	motor[right] = 10;
+	wait1Msec(5000);
+
+	motor[left] = -30;
+	motor[right] = -30;
+	wait1Msec(1000;
 
 
 
